f7ea285e87e77fe0b9e8bb6f5a607521756c482b
Nicolas Mollo <nicolas.mollo.alanguia@cftmail.cl>
2024-10-14 00:20:06 -0300

    casi pierdo todo mi trabajo, pero lo recupere

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { RegistrateComponent } from './registrate/registrate.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {
@@ -20,19 +17,3 @@ export const routes: Routes = [
         component: RegistrateComponent
     }
 ];
-
-@NgModule({
-    declarations: [
-      AppComponent,
-      InicioComponent,
-      ContactoComponent,
-      RegistrateComponent
-    ],
-    imports: [
-      BrowserModule,
-      RouterModule.forRoot(routes)
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
-  })
-  export class AppModule { }
