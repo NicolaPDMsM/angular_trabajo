b5dac68c28d0dc2520dbb39a53aa487be00f04df
Nicolas Mollo <nicolas.mollo.alanguia@cftmail.cl>
2024-10-13 23:55:38 -0300

    Primera version de este proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { RegistrateComponent } from './registrate/registrate.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: InicioComponent,
+    },
+    {
+        path: "contactanos",
+        component: ContactoComponent
+    },
+    {
+        path: "registro",
+        component: RegistrateComponent
+    }
+];
+
+@NgModule({
+    declarations: [
+      AppComponent,
+      InicioComponent,
+      ContactoComponent,
+      RegistrateComponent
+    ],
+    imports: [
+      BrowserModule,
+      RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
